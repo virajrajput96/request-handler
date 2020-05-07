8871171650869452366b218d638e5245b1030aab
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-07 16:39:29 +0530

    Update access.lua

--- kong/plugins/request-handler/access.lua
+++ kong/plugins/request-handler/access.lua
@@ -1,7 +1,7 @@
 local _M = {}
 
 function _M.execute()
-  if (string.match(str, ";") or string.match(kong.request.get_scheme(),"$") or string.match(kong.request.get_host(),"$") or string.match(kong.request.get_port(),"$") or string.match(kong.request.get_forwarded_scheme(),"$") or string.match(kong.request.get_forwarded_host(),"$") or string.match(kong.request.get_forwarded_port(),"$") or string.match(kong.request.get_http_version(),"$") or string.match(kong.request.get_method(),"$") or string.match(kong.request.get_path(),"$") or string.match(kong.request.get_raw_query(),"$") or string.match(kong.request.get_raw_query(),"$")) then
+  if (string.match(kong.request.get_scheme(),"$") or string.match(kong.request.get_host(),"$") or string.match(kong.request.get_port(),"$") or string.match(kong.request.get_forwarded_scheme(),"$") or string.match(kong.request.get_forwarded_host(),"$") or string.match(kong.request.get_forwarded_port(),"$") or string.match(kong.request.get_http_version(),"$") or string.match(kong.request.get_method(),"$") or string.match(kong.request.get_path(),"$") or string.match(kong.request.get_raw_query(),"$") or string.match(kong.request.get_raw_query(),"$")) then
   print ("400,Bad Request")
   print ("Request can not contain '$'")
 else
