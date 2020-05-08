bd3763182309eae7d818151792a977b7c9cd4984
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-08 23:32:42 +0530

    Update handler.lua

--- kong/plugins/request-handler/handler.lua
+++ kong/plugins/request-handler/handler.lua
@@ -12,11 +12,11 @@ function RequestHandler:access()
   RequestHandler.super.access()
   
  if (string.match(kong.request.get_scheme(),"$") or string.match(kong.request.get_host(),"$") or string.match(kong.request.get_port(),"$") or string.match(kong.request.get_forwarded_scheme(),"$") or string.match(kong.request.get_forwarded_host(),"$") or string.match(kong.request.get_forwarded_port(),"$") or string.match(kong.request.get_http_version(),"$") or string.match(kong.request.get_method(),"$") or string.match(kong.request.get_path(),"$") or string.match(kong.request.get_raw_query(),"$") or string.match(kong.request.get_raw_query(),"$")) then
-  print ("400,Bad Request")
-  print ("Request can not contain '$'")
+  ngx.log(ngx.ERR, "Request")
+  ngx.header["Request-handler"] = "Successful Request!!!"
 else
-  print ("200,OK")
-    print ("Request Sucessful.")
+  ngx.log(ngx.ERR, "Bad Request")
+  ngx.header["Request-Handler"] = "Bad Request!!!"
 end
   
 end
