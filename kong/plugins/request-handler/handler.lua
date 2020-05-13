ad6c3519e5c234d237bef368ef1bf9f0a01b76ba
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-13 15:34:36 +0530

    Update handler.lua

--- kong/plugins/request-handler/handler.lua
+++ kong/plugins/request-handler/handler.lua
@@ -3,14 +3,14 @@ requestHandler.PRIORITY = 7
 requestHandler.version = "0.1-1"
 
 local err,ok
-scheme=kong.request.get_scheme()
+
+
+function requestHandler:log(conf)
+ scheme=kong.request.get_scheme()
 f_scheme=kong.request.get_forwarded_scheme()
 forwarded_host=kong.request.get_forwarded_host()
 method=kong.request.get_method()
 path=kong.request.get_path()
-
-function requestHandler:log(conf)
- 
  if not string.match(scheme,"$") or string.match(host,"$") or string.match(port,"$") or string.match(f_scheme,"$") or string.match(forwarded_host(),"$") or string.match(method,"$") or string.match(path,"$") then
    return kong.log.debug("Successful Request")
  else
