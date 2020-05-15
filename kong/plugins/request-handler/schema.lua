9cd93bc68184379a6d842516d1cd8d7a9a085ea6
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-15 22:33:17 +0530

    Update schema.lua

--- kong/plugins/request-handler/schema.lua
+++ kong/plugins/request-handler/schema.lua
@@ -9,6 +9,10 @@ return {
         fields = {
           { host = typedefs.host({ required = true }) },
           { port = typedefs.port({ required = true }) },
+          { timeout = { type = "number", default = 10000 }, },
+          { keepalive = { type = "number", default = 60000 }, },
+          { tls = { type = "boolean", default = false }, },
+          { tls_sni = { type = "string" }, },
           },
     }, }, },
   }
