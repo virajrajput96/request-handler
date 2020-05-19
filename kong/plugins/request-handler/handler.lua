6494699859294eec0990150194be89858c71a1df
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-19 22:26:21 +0530

    Update handler.lua

--- kong/plugins/request-handler/handler.lua
+++ kong/plugins/request-handler/handler.lua
@@ -1,16 +1,21 @@
-local requestHandler = {}
+local BasePlugin = require "kong.plugins.base_plugin"
+local requestHandler = BasePlugin:extend()
+
+
 requestHandler.PRIORITY = 2000
 requestHandler.version = "0.1-1"
 
+function HelloWorldHandler:new()
+  HelloWorldHandler.super.new(self, "request-handler")
+end
 
-function requestHandler:log(conf)
-
-
+function HelloWorldHandler:access(conf)
+  HelloWorldHandler.super.access(self)
 
    ngx.log(ngx.ERR, "successful request: ")
    ngx.header["request-handler"] = "Request-handler request successful!!!!!"
  
-end
+
 end
 
 return requestHandler
