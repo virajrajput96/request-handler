9e1e98474d9c1efd0ac3b4166437beb9080b4fa6
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-19 21:29:46 +0530

    Update handler.lua

--- kong/plugins/request-handler/handler.lua
+++ kong/plugins/request-handler/handler.lua
@@ -1,11 +1,11 @@
 local requestHandler = {}
 requestHandler.PRIORITY = 2000
 requestHandler.version = "0.1-1"
-
+local ok
 
 function requestHandler:log(conf)
 
-local ok
+
 
    ngx.log(ngx.ERR, "[request-handler] successful request: ", ok)
    ngx.header["request-handler"] = "Request-handler request successful!!!!!"
