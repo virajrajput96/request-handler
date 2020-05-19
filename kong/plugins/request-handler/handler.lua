9ede25107326309001f52b115aedc1cc9f64f1db
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-19 21:05:32 +0530

    Update handler.lua

--- kong/plugins/request-handler/handler.lua
+++ kong/plugins/request-handler/handler.lua
@@ -1,23 +1,15 @@
 local requestHandler = {}
-requestHandler.PRIORITY = 7
+requestHandler.PRIORITY = 2000
 requestHandler.version = "0.1-1"
 
-local ok,err
 
 function requestHandler:log(conf)
 
-scheme=kong.request.get_scheme()
-f_scheme=kong.request.get_forwarded_scheme()
-forwarded_host=kong.request.get_forwarded_host()
-method=kong.request.get_method()
-path=kong.request.get_path()
 
- if not string.match(scheme,"$") or string.match(host,"$") or string.match(port,"$") or string.match(f_scheme,"$") or string.match(forwarded_host(),"$") or string.match(method,"$") or string.match(path,"$") then
+
    ngx.log(ngx.ERR, "[request-handler] successful request: ", ok)
    ngx.header["request-handler"] = "Request-handler request successful!!!!!"
- else
-   ngx.log(ngx.ERR, "[request-handler] request conatain $: ", err)
-  ngx.header["request-handler"] = "Request-handler request Failed!!!!!"
+ 
 end
 end
 
