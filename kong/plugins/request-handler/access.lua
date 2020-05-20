163d373005de6afd634906e40741f6d6eae4e474
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-20 16:35:34 +0530

    Update access.lua

--- kong/plugins/request-handler/access.lua
+++ kong/plugins/request-handler/access.lua
@@ -1,8 +1,14 @@
 local _M = {}
 
 function _M.execute(conf)
+host= kong.request.get_host()
+  if not string.match(host,"$") then
   ngx.log(ngx.ERR, "successful request: ")
    ngx.header["request-handler"] = "Request-handler request successful!!!!!"
+    else
+    ngx.log(ngx.ERR,"--------------ByeDuniya-----------")
+    ngx.header["request-handler"] = "Bad request!!!!!"
+  end
   
 end
 return _M
