2066ab05bf17a7ce6da657d6a3c9062ceba38d15
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-05-28 23:26:30 +0530

    Update schema.lua

--- kong/plugins/request-handler/schema.lua
+++ kong/plugins/request-handler/schema.lua
@@ -2,19 +2,7 @@ local typedefs = require "kong.db.schema.typedefs"
 
 
 return {
-  
-  name = "request-handler",
+  no_consumer = true,
   fields = {
-        { protocols = typedefs.protocols_http },
-         { config = {
-        type = "record",
-        fields = {
-          { host = typedefs.host({ required = true }), },
-	        { port = typedefs.port({ required = true }), },
-          { tls = { type = "boolean", default = false }, },
-          { tls_sni = { type = "string" }, },
-          },
-        },
-      },
-    }
+           }
   }
